61dc6127d90d945e487baef257d6f17675d2338c
HtooKhant02 <vexixongaming02@gmail.com>
2022-09-08 21:01:40 +0630

    begin active to nav

---
+++ main.js
@@ -0,0 +1,17 @@
+import './style.scss';
+import './node_modules/bootstrap/dist/js/bootstrap.bundle';
+import 'animate.css';
+import Typed from 'typed.js';
+
+//typed.js
+let options = {
+    strings: ['GOOD FOOD..', 'GOOD MOOD !'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 900,
+    loop:true
+  };
+
+let typed = new Typed('.element', options);
+
+//wow.js
